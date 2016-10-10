435c3799b067b9cdc360388c82e4c37478aa8c76
MELWIN JOSE <melwin.jose.1991@gmail.com>
2016-10-10 11:32:59 -0400

    Cracking :: TripleStep & Insertion

---
+++ src/melwin/codinginterview/RecursionAndDP/TripleStep.java
@@ -0,0 +1,35 @@
+package melwin.codinginterview.RecursionAndDP;
+
+import java.util.Arrays;
+
+/*
+ * Chapter 8	:	Recursion and Dynamic Programming
+ * Question 8.1	:	Triple Step
+ */
+public class TripleStep {
+
+	private static int[] memo;
+
+	private static int autoPlay(int n) {
+		if (n <= 0) {
+			if (n == 0)
+				return 1;
+			return 0;
+		}
+
+		if (memo[n] != -1)
+			return memo[n];
+
+		memo[n] = autoPlay(n - 1) + autoPlay(n - 2) + autoPlay(n - 3);
+		return memo[n];
+	}
+
+	public static void main(String[] args) {
+		int n = 10;
+		memo = new int[n + 1];
+		Arrays.fill(memo, -1);
+
+		System.out.println(autoPlay(n));
+	}
+
+}
