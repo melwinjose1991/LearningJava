a726dc28c4a5cf9ce9eccdedd17e8edc9530bf34
MELWIN JOSE <melwin.jose.1991@gmail.com>
2016-10-22 12:39:02 -0400

    DesignPatterns :: ObserverPattern

---
+++ src/melwin/learning/collections/queue/ExploringLinkedList.java
@@ -0,0 +1,20 @@
+package melwin.learning.collections.queue;
+
+import java.util.LinkedList;
+
+public class ExploringLinkedList {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> ll = new LinkedList<Integer>();
+		
+		ll.add(1);
+		ll.add(1);
+		ll.add(2);
+		ll.add(3);
+		
+		while(ll.peek()!=null){
+			System.out.println(ll.poll());
+		}
+	}
+
+}
