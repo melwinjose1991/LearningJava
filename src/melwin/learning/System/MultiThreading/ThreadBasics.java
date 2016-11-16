e6c529c792d0d7b34b2ffbc2ecd7944d3a883900
MELWIN JOSE <melwin.jose.1991@gmail.com>
2016-11-15 22:25:01 -0500

    ReOrg

---
+++ src/melwin/learning/System/MultiThreading/ThreadBasics.java
@@ -0,0 +1,43 @@
+package melwin.learning.System.MultiThreading;
+
+public class ThreadBasics {
+
+	private static int counter;
+
+	private synchronized static void incrementCounter() {
+		counter++;
+	}
+
+	static class MyThread extends Thread {
+		@Override
+		public void run() {
+			for (int i = 0; i < 10000; i++) {
+				incrementCounter();
+			}
+		}
+	}
+
+	static class MyRunnable implements Runnable {
+
+		@Override
+		public void run() {
+			for (int i = 0; i < 10000; i++) {
+				incrementCounter();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread runner = new Thread(new MyRunnable());
+		MyThread threader = new MyThread();
+
+		runner.start();
+		threader.start();
+
+		runner.join();
+		threader.join();
+
+		System.out.println(counter);
+	}
+
+}
