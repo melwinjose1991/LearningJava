b027d5101d352bfb79cc130b469895709f8346d3
MELWIN JOSE <melwin.jose.1991@gmail.com>
2017-03-06 02:00:23 -0500

    Thread Excercise :: OneTwoThree

--- src/melwin/learning/System/MultiThreading/Exercise/OneTwoThree.java
+++ src/melwin/learning/System/MultiThreading/Exercise/OneTwoThree.java
@@ -1,8 +1,9 @@
 package melwin.learning.System.MultiThreading.Exercise;
 
+// ||ONE TWO THREE|| ||EXCERCIS||
 public class OneTwoThree {
 
-	private static int loops = 3;
+	private static int number_of_threads = 3;
 	private static Object lock = new Object();
 	private static int thread_turn = 1;
 
@@ -15,7 +16,7 @@ public class OneTwoThree {
 
 		@Override
 		public void run() {
-			for (int i = 0; i < loops; i++) {
+			for (int i = 0; i < number_of_threads; i++) {
 				synchronized (lock) {
 					while (thread_turn != my_number) {
 						try {
@@ -25,7 +26,7 @@ public class OneTwoThree {
 						}
 					}
 					System.out.println(my_number);
-					thread_turn = thread_turn == 3 ? 1 : thread_turn + 1;
+					thread_turn = thread_turn == number_of_threads ? 1 : thread_turn + 1;
 					lock.notifyAll();
 				}
 			}
@@ -34,7 +35,7 @@ public class OneTwoThree {
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		for (int i = 1; i <= loops; i++) {
+		for (int i = 1; i <= number_of_threads; i++) {
 			new Thread(new Printer(i)).start();
 		}
 
