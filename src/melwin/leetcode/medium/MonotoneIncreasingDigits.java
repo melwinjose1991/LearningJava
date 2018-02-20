aac8010aea9f37d1bfebc1adc1ff6108f8930e35
Melwin Jose <melwin.jose.1991@gmail.com>
2018-02-19 21:32:42 -0500

    Leetcode :: FB Google Prep

---
+++ src/melwin/leetcode/medium/MonotoneIncreasingDigits.java
@@ -0,0 +1,32 @@
+package melwin.leetcode.medium;
+
+// 738. Monotone Increasing Digits - O(N)
+
+public class MonotoneIncreasingDigits {
+	
+	public int monotoneIncreasingDigits(int N) {
+
+		if (N <= 9)
+			return N;
+		
+		char[] x = String.valueOf(N).toCharArray();
+
+		int mark = x.length;
+		for (int i = x.length - 1; i > 0; i--) {
+			if (x[i] < x[i - 1]) {
+				mark = i - 1;
+				x[i - 1]--;
+			}
+		}
+		
+		for (int i = mark + 1; i < x.length; i++) {
+			x[i] = '9';
+		}
+		
+		return Integer.parseInt(new String(x));
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
